8ece6a96341c932962ac5f6ccaf740f69e68aa7d
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-10-05 19:44:09 +0530

    [Tirupathi] Add. Validated Last Name UC2

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -5,23 +5,35 @@ namespace UserRegistration
 {
     class Program
     {
+        public static string validateFunction(Regex regex,string name)
+        {
+            while (true)
+            {
+                Console.Write("Enter "+name+" : ");
+                string userName = Console.ReadLine();
+
+                if (regex.IsMatch(userName))
+                {
+                    Console.WriteLine("\n\n********\n\n" + name+" is valid..." + "\n\n********\n\n");
+                    return userName;
+                }
+                else
+                {
+                    Console.WriteLine("\n\n********\n\n Invalid " + name+ " ... Please enter atleast three characters starting with a Capital letter (Eg.: Sam,Jack)"+ "\n\n********\n\n");
+                }
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to user registration form...");
 
-            Console.Write("Enter user name : ");
-            string userName = Console.ReadLine();
-            string pattern = @"^[A-Z][A-Za-z0-9_]{2,}$";
+            string pattern = @"^[A-Z][A-Za-z]{2,}$";
             Regex regex = new Regex(pattern);
 
-            if (regex.IsMatch(userName))
-            {
-                Console.WriteLine("Username is valid...");
-            }
-            else
-            {
-                Console.WriteLine("Invalid Username ... Please enter atleast three characters starting with a Capital letter (Eg.: Sam,Tom_1)");
-            }
+            String firstName = validateFunction(regex,"First Name");
+            String lastName = validateFunction(regex,"Last Name");
+
+            Console.WriteLine("\n\n********\n\nYour full name is : " + firstName + " " + lastName+ "\n\n********\n\n");
 
         }
     }
