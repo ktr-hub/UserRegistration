3d4c1c8906932c6f01c7c3a9637f08baa4ad3483
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-10-05 19:25:47 +0530

    [Tirupathi] Add. User name validated UC1

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace UserRegistration
 {
@@ -7,6 +8,21 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to user registration form...");
+
+            Console.Write("Enter user name : ");
+            string userName = Console.ReadLine();
+            string pattern = @"^[A-Z][A-Za-z0-9_]{2,}$";
+            Regex regex = new Regex(pattern);
+
+            if (regex.IsMatch(userName))
+            {
+                Console.WriteLine("Username is valid...");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Username ... Please enter atleast three characters starting with a Capital letter (Eg.: Sam,Tom_1)");
+            }
+
         }
     }
 }
